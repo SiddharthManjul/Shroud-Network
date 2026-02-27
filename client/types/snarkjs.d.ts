e10ab906ea36632747eea41ab7f5225ca32a19ae
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-02-27 07:56:05 +0530

    types: snarkjs

---
+++ client/types/snarkjs.d.ts
@@ -0,0 +1,14 @@
+declare module "snarkjs" {
+  export const groth16: {
+    fullProve(
+      input: Record<string, any>,
+      wasmPath: string,
+      zkeyPath: string
+    ): Promise<{ proof: any; publicSignals: string[] }>;
+    verify(
+      verificationKey: any,
+      publicSignals: string[],
+      proof: any
+    ): Promise<boolean>;
+  };
+}
