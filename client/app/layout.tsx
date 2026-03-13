10fc7a3adc2ee7ff6ec2fb8783cb0a96d6587256
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-03-13 23:33:46 +0530

    layout updated with google analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -22,6 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CBTYRZLX3N"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CBTYRZLX3N');
+          `}
+        </Script>
+      </head>
       <body className={`${spaceGrotesk.variable} font-space antialiased`}>
         <Providers>{children}</Providers>
       </body>
